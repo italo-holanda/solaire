326b744dcd89ee79d872876a75689da733c4fa9f
= <italo.holanda@hub.live>
2025-07-01 07:09:35 -0300

    feature: includes the create-publication-preview-usecase

---
+++ backend/core/domain/entities/publication.py
@@ -0,0 +1,26 @@
+from typing import List, Literal
+
+from backend.core.domain.entities.category import Category
+from backend.core.domain.entities.entity import Entity
+
+PublicationFormat = Literal[
+    "linkedin_post",
+    "blog_post",
+    "short_video",
+    "long_video"
+]
+
+PublicationStage = Literal[
+    "preview",
+    "ready"
+]
+
+
+class Publication(Entity):
+    id: str
+    title: str
+    content: str
+    categories: List[Category]
+    outlining: List[str]
+    format: PublicationFormat
+    stage: PublicationStage
