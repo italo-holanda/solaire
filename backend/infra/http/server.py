e9266b5d5e8507ff1963bff1a08b75a6b2f0a1cc
= <italo.holanda@hub.live>
2025-07-04 08:51:27 -0300

    feat: includes health check route

--- backend/infra/http/server.py
+++ backend/infra/http/server.py
@@ -1,7 +1,10 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
+from backend.infra.http.routes import common
 
 load_dotenv(dotenv_path='.env')
 
 app = FastAPI()
+
+app.include_router(common.router)
