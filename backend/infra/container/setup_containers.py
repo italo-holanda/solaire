071f890238e6964b4378b02fa6e7af02683b0c81
= <italo.holanda@hub.live>
2025-07-09 06:29:30 -0300

    chore: add 'create-thought' route

--- backend/infra/container/setup_containers.py
+++ backend/infra/container/setup_containers.py
@@ -1,3 +1,4 @@
+from backend.core.thought.domain.repositories.thought_vector_store import ThoughtVectorStoreInterface
 from backend.infra.container.container import Container
 
 from backend.core.category.domain.repositories.category_repository import CategoryRepositoryInterface
@@ -7,6 +8,7 @@ from backend.core.thought.domain.repositories.thought_repository import ThoughtR
 from backend.infra.databases.relational.repositories.category_repository import CategoryRepository
 from backend.infra.databases.relational.repositories.publication_repository import PublicationRepository
 from backend.infra.databases.relational.repositories.thought_repository import ThoughtRepository
+from backend.infra.databases.vectorial.thought_vector_store import ThoughtVectorStore
 
 
 def setup_di_container():
@@ -16,3 +18,4 @@ def setup_di_container():
     Container.register(CategoryRepositoryInterface, CategoryRepository)
     Container.register(ThoughtRepositoryInterface, ThoughtRepository)
     Container.register(PublicationRepositoryInterface, PublicationRepository)
+    Container.register(ThoughtVectorStoreInterface, ThoughtVectorStore)
--- backend/infra/http/routes/thoughts.py
+++ backend/infra/http/routes/thoughts.py
@@ -4,6 +4,7 @@ from typing import List
 from fastapi import APIRouter, HTTPException
 
 from backend.core.common.domain.exceptions.application_exception import ApplicationException
+from backend.core.thought.application.usecases.create_thought_usecase import CreateThoughtDTO, CreateThoughtUsecase
 from backend.core.thought.application.usecases.list_thoughts_usecase import ListThoughtsDTO, ListThoughtsUsecase
 from backend.core.thought.domain.entities.thought import Thought
 
@@ -22,6 +23,24 @@ async def get_thoughts(search_request: ListThoughtsDTO) -> List[Thought]:
 
     try:
         result = usecase.execute(search_request)
+        return result
+    except ApplicationException as ae:
+        raise HTTPException(status_code=ae.code, detail=ae.message)
+    except Exception:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail="Internal server error")
+
+
+@router.post("/")
+async def create_thought(dto: CreateThoughtDTO) -> List[Thought]:
+    """
+    Create new thought
+    """
+
+    usecase = Container.resolve(CreateThoughtUsecase)
+
+    try:
+        result = usecase.execute(dto)
         return result
     except ApplicationException as ae:
         raise HTTPException(status_code=ae.code, detail=ae.message)
