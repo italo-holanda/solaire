ac29f4922ee840c74038c7f0d96db52897717475
Italo Holanda <italoholanda@proton.me>
2025-07-12 13:21:22 -0300

    feat: adds list-categories route

--- backend/infra/http/server.py
+++ backend/infra/http/server.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
-from backend.infra.http.routes import common, thoughts
+from backend.infra.http.routes import common, thoughts, categorires
 
 load_dotenv(dotenv_path='.env')
 
@@ -9,3 +9,4 @@ app = FastAPI()
 
 app.include_router(common.router)
 app.include_router(thoughts.router, prefix="/thoughts", tags=["thoughts"])
+app.include_router(categorires.router, prefix="/categories", tags=["categories"])
