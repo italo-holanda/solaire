8551030fa3ae91ca4f0b574f150271e23752c303
Italo Holanda <italoholanda@proton.me>
2025-07-18 18:10:50 -0300

    chore: includes initial frontend cfg

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button'
+
+import "./App.css"
+
+export default function App() {
+  return (
+    <div className='bg-stone-950'>
+      <Button>Hello</Button>
+    </div>
+  )
+}
+
+
