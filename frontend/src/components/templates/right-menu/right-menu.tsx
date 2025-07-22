a612075465f701dc2d0b911286180d01037ff357
= <italo.holanda@hub.live>
2025-07-22 06:36:16 -0300

    fix: fix right-menu border

--- frontend/src/components/templates/right-menu/right-menu.tsx
+++ frontend/src/components/templates/right-menu/right-menu.tsx
@@ -2,7 +2,7 @@ import { Separator } from "@/components/atoms/separator";
 
 export function RightMenu() {
   return (
-    <aside className="bg-stone-950 h-screen w-xs p-2 border-r-1 border-stone-800">
+    <aside className="bg-stone-950 h-screen w-xs p-2 border-l-1 border-stone-800">
       <div className="h-20 flex items-center justify-center">
         <code className="text-xs text-stone-500">v1.0 (DEMO)</code>
       </div>
