60dcbe5b374f850ef3db810d8eb6d823249a1395
= <italo.holanda@hub.live>
2025-07-22 06:13:45 -0300

    feat: add right-menu component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 
 import { LeftMenu } from './components/templates/left-menu/left-menu'
+import { RightMenu } from './components/templates/right-menu/right-menu'
 
 export default function App() {
   return (
-    <div className='h-screen'>
-      <div>
+    <div className='bg-stone-900 h-screen'>
+      <div className='flex justify-between'>
         <LeftMenu />
+        <div></div>
+        <RightMenu />
       </div>
     </div>
   )
---
+++ frontend/src/components/templates/right-menu/right-menu.tsx
@@ -0,0 +1,15 @@
+import { Separator } from "@/components/atoms/separator";
+
+export function RightMenu() {
+  return (
+    <aside className="bg-stone-950 h-screen w-xs p-2 border-r-1 border-stone-800">
+      <div className="h-20 flex items-center justify-center">
+        <code className="text-xs text-stone-500">v1.0 (DEMO)</code>
+      </div>
+
+      <Separator />
+
+      <div className="py-8 px-2"></div>
+    </aside>
+  );
+}
