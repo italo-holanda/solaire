f9681e68fc8d9240eceffc4aae22f088cd9d747d
Italo Holanda <italoholanda@proton.me>
2025-07-28 06:03:48 -0300

    fix: fix thought-history scroll

--- frontend/src/components/templates/thought-history/thought-history.tsx
+++ frontend/src/components/templates/thought-history/thought-history.tsx
@@ -11,7 +11,7 @@ export function ThoughtHistory() {
       <Separator />
 
       <div className="max-w-2xl px-6 mx-auto flex flex-col flex-1 min-h-0">
-        <div className="pl-4 flex-1 min-h-0 flex flex-col overflow-scroll">
+        <div className="pl-4 flex-1 min-h-0 flex flex-col overflow-y-scroll">
           {/* Creates a top gap  */}
           <div className="min-h-8 w-full">
             <span className="text-transparent">...</span>
